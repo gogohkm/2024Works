d8dfe7b63cba5990a0ab215be986e2c46c0eb254
gogohkm <femanal@naver.com>
2024-05-20 01:01:44 +0900

    main.py를 책 page 64에 맞도록 작성함

--- Coding_works/bitcoin_trading_v01/main.py
+++ Coding_works/bitcoin_trading_v01/main.py
@@ -1,12 +1,20 @@
-import requests
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget  # 어플리케이션 핸들러와 빈 GUI 위젯
 
 
-def get_bitcoin_price():
-    url = "https://api.upbit.com/v1/ticker?markets=BTC-KRW"
-    response = requests.get(url)
-    data = response.json()
-    return data[0]['trade_price']
+class Calculator(QWidget): # QWidget을 상속받아 Calculator 클래스를 정의
 
-def printhkm():
-    print("hello")
+    def __init__(self):
+        super().__init__()  # 부모 클래스 초기화
+        self.initUI()       # 나머지 초가화는 initUI() 메서드에서 처리
+    def initUI(self):
+        self.setWindowTitle('Calculator')     # 타이틀바에 'Calculator' 표시
+        self.setGeometry(100, 100, 300, 400)  # 위치와 크기 설정
+        self.show()                           # 위젯을 화면에 표시
 
+
+if __name__ == '__main__':            # pyqt는 애플리케이션당 하나의 QApplication 객체만을 생성할 수 있음
+    app = QApplication(sys.argv)      # QApplication 객체 생성
+    calculator = Calculator()         # Calculator 윈도우 객체 생성
+    sys.exit(app.exec_())             # 이벤트 루프 실행
+    
