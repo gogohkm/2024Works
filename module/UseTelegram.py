d31fc02b1cde028bf6498cb52dc5bf0762c73398
Han Kyong Min <106009940+gogohkm@users.noreply.github.com>
2024-09-18 01:54:49 +0900

    코드 초기세팅05 텔레그램 메시지를 보낼수 있도록 예제 추가

--- module/UseTelegram.py
+++ module/UseTelegram.py
@@ -4,7 +4,7 @@ import os
 import sys
 import logging
 import traceback
-# python-telegram-bot 라이브러리 사용시 2.0버전부터는 아래와 같이 asyncio를 인포트해야 한다. 
+# python-telegram-bot 라이브러리 사용시 2.0버전부터는 아래와 같이 asyncio를 인포트해야 한다.
 import asyncio
 # 공통 모듈 Import
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
